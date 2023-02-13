b9060bd7c010ff1d24356bfe69d9317ca6cd6ae7
Rawllings Kiprop <rutojr01@gmail.com>
2023-02-13 21:15:00 +0300

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+class Student < User
+    
+
+    def initialize
+        @knowledge = []
+    end
+   
+   def learn(knowledge)
+     @knowledge = knowledge
+   end
+
+   def knowledge
+    @knowledge
+   end
+
+   def self.knowledge
+    @@knowledge
+   end
 
 end
